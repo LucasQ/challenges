c7a0febb7b48c514ec233543ba246418783bcd85
LucasQ <mallevo13@gmail.com>
2020-12-05 20:17:28 -0300

    estrutura do projeto + começo do 1º desafio

---
+++ src/screens/cuboChallenge/Cubo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavCubo from './NavCubo';
+
+export default function Cubo() {
+  return (
+    <div>
+      <NavCubo />
+      <div className="container mx-auto">
+        <div className="flex justify-center mx-32">
+          <h1>a</h1>
+          <h1>a</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
