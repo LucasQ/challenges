d60aa4922644a8c308533718f0ff55651f872728
LucasQ <mallevo13@gmail.com>
2020-12-21 07:30:12 -0300

    table

--- src/screens/cuboChallenge/Cubo.js
+++ src/screens/cuboChallenge/Cubo.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import NavCubo from './NavCubo';
+import Table from './Table';
 
 export default function Cubo() {
   return (
     <div>
       <NavCubo />
       <div className="container mx-auto">
-        <div className="flex justify-center mx-32">
-          <h1>a</h1>
-          <h1>a</h1>
+        <div className="flex justify-center mx-32 flex-col">
+          <div className=" flex-1 flex-row text-center mt-14">
+            <h1 className="font-bold text-4xl	">DATA</h1>
+            <h3 className="mt-4 text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </h3>
+          </div>
+          <div className="flex-2 mt-14">
+            <Table />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/screens/cuboChallenge/Table.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function Table() {
+  return (
+    <table className="table-fixed border-collapse border">
+      <thead>
+        <tr>
+          <th className="border px-3 py-1 border-gray-300"> </th>
+          <th className="border px-16 py-1 border-gray-300">First name</th>
+          <th className="border px-24 py-1 border-gray-300">Last name</th>
+          <th className="border p-6 py-1 border-gray-300">Participation</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="border px-3 py-1 border-gray-300">1</td>
+          <td className="border border-gray-300 pl-2">Intro to CSS</td>
+          <td className="border border-gray-300 pl-2">Adam</td>
+          <td className="border border-gray-300 text-center">858</td>
+        </tr>
+        <tr>
+          <td className="border px-3 py-1 border-gray-300">2</td>
+          <td className="border border-gray-300 pl-2">A Long and</td>
+          <td className="border border-gray-300 pl-2">Adam</td>
+          <td className="border border-gray-300 text-center">112</td>
+        </tr>
+        <tr>
+          <td className="border px-3 py-1 border-gray-300">3</td>
+          <td className="border border-gray-300 pl-2">Intro to JavaScript</td>
+          <td className="border border-gray-300 pl-2">Chris</td>
+          <td className="border border-gray-300 text-center">1,280</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
